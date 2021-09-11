3534dd0bffd8c12b65f5a5f92c5fc0f12fad8edd
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-09-11 01:42:07 -0400

    Code added

--- src/Lab04.java
+++ src/Lab04.java
@@ -11,27 +11,33 @@ public class Lab04 {
         // with text input
 
         //              class        function
-        inputWord = JOptionPane.showInputDialog(null,
-                "Enter a word in all lower case:");
 
-        // if the inputWord is contained within wordArray return true
-        if (wordIsThere(inputWord, wordArray))
-            isOrIsNot = "is"; // set to is if the word is on the list
-        else
-            isOrIsNot = "is not"; // set to is not if the word is not on the list
+        while (true) {
+            inputWord = JOptionPane.showInputDialog(null,
+                    "Enter a word in all lower case:");
+            if (inputWord.equals("STOP"))
+                System.exit(0);
 
-        // Output to a JOptionPane window whether the word is on the list or not
-        JOptionPane.showMessageDialog(null,
-                "The word " + inputWord + " " + isOrIsNot + " on the list.");
 
-        // step 3 in main function.....
+            // if the inputWord is contained within wordArray return true
+            if (wordIsThere(inputWord, wordArray))
+                isOrIsNot = "is"; // set to is if the word is on the list
+            else
+                isOrIsNot = "is not"; // set to is not if the word is not on the list
+
+            // Output to a JOptionPane window whether the word is on the list or not
+            JOptionPane.showMessageDialog(null,
+                    "The word " + inputWord + " " + isOrIsNot + " on the list.");
+        }
     } //main
 
     public static boolean wordIsThere(String findMe, String[] theList) {
         // find if findMe = elements of array
-
-        // s1.equals(s2), "Hello".equals("hello") = f;
-        // s1.equalsIgnoreCase(s2), "Hello".equalsIgnoreCase("hello") = t
+        for (String s : theList) {
+            if (findMe.equals(s)) {
+                return true;
+            }
+        }
 
         return false;
     } // wordIsThere
