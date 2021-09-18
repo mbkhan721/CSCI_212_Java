259d4e06bf839ed7f5f137e1ad27fa30e428722a
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-09-18 16:39:05 -0400

    Code added

--- src/Lab05.java
+++ src/Lab05.java
@@ -1,39 +1,43 @@
 // Muhammad Khan - Lab 05
 
-import javax.swing.*;
-import java.io.BufferedReader;
+import java.io.BufferedReader;  // Reads text from character input stream
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
 public class Lab05 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         String[] wordArray = { "hello", "goodbye", "cat", "dog",
                 "red", "green", "sun", "moon" };
         String isOrIsNot, inputWord;
 
-        // This line asks the user for input by popping out a single window
-        // with text input
-
-        //              class        function
-
-        while (true) {
-            inputWord = JOptionPane.showInputDialog(null,
-                    "Enter a word in all lower case:");
-            if (inputWord.equals("STOP"))
-                System.exit(0);
+        // Read input file
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(args[0])));
+        String line = br.readLine();
+        System.out.println(line);
 
 
-            // if the inputWord is contained within wordArray return true
-            if (wordIsThere(inputWord, wordArray))
-                isOrIsNot = "is"; // set to is if the word is on the list
-            else
-                isOrIsNot = "is not"; // set to is not if the word is not on the list
-
-            // Output to a JOptionPane window whether the word is on the list or not
-            JOptionPane.showMessageDialog(null,
-                    "The word " + inputWord + " " + isOrIsNot + " on the list.");
-        }
+//        // This line asks the user for input by popping out a single window
+//        // with text input
+//
+//        //              class        function
+//        while (true) {
+//            inputWord = JOptionPane.showInputDialog(null,
+//                    "Enter a word in all lower case:");
+//            if (inputWord.equals("STOP"))
+//                System.exit(0);
+//
+//
+//            // if the inputWord is contained within wordArray return true
+//            if (wordIsThere(inputWord, wordArray))
+//                isOrIsNot = "is"; // set to is if the word is on the list
+//            else
+//                isOrIsNot = "is not"; // set to is not if the word is not on the list
+//
+//            // Output to a JOptionPane window whether the word is on the list or not
+//            JOptionPane.showMessageDialog(null,
+//                    "The word " + inputWord + " " + isOrIsNot + " on the list.");
+//        }
     } //main
 
     public static boolean wordIsThere(String findMe, String[] theList) {
