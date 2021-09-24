1e92f4355e3f3528a7cc86ce1a9fdf78c338afaf
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-09-23 23:03:38 -0400

    Code added

--- src/Lab06.java
+++ src/Lab06.java
@@ -1,18 +1,61 @@
+import javax.swing.*;
 import java.io.*;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
 
 public class Lab06 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(args[0])));
-        String line = br.readLine();
-        while (line != null) {
-            System.out.println(line);
-            line = br.readLine();
-        }
-        br.close();
+    public static void main(String[] args) {
+        String filename = args[0];
+        int length = 10;
+        int[] array = new int[length];
+        inputFromFile(array,filename);
 
-        //int sum = sum (array, array.length);
-        //int average = average(array, array.length);
+        //printing array
+        for(int i=0; i < length && array[i] != 0; i++)
+            System.out.print(array[i] + " ");
+
+
+        int sum = sum (array, array.length);
+        JOptionPane.showMessageDialog(null,
+                "The sum of all int  " + sum);
+
+        float average = average(array, array.length);
+        JOptionPane.showMessageDialog(null,
+                "The average is  " + average);
     }
 
+    public static void inputFromFile(int[] array, String filename) {
+        Scanner input;
+        try{
+            FileReader fr = new FileReader(filename);
+            input = new Scanner(fr);
+        }
+        catch(FileNotFoundException e){
+            System.err.println("File not Found!!");
+            return;
+        }
+        int index = 0;
+        while(input.hasNext()) {
+           array[index] = Integer.parseInt(input.next());
+           index++;
+        }
+    }
 
+    public static int sum (int[] myArray, int myArraySize) {
+        myArraySize = myArray.length;
+        int sum = 0;
+        for (int i = 0; i < myArraySize; i++) {
+            sum += myArray[i];
+        }
+        return sum;
+    }
+
+    public static float average (int[] myArray, int myArraySize){
+        myArraySize = myArray.length;
+        float average = 0.0f;
+        for (int i = 0; i < myArraySize; i++) {
+            average += myArray[i];
+        }
+        return average / myArraySize;
+    }
 }
