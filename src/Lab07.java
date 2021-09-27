2669c08d5c020dfcdf402c1bba2c49fdc15d21d8
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-09-27 11:50:51 -0400

    Code added

--- src/Lab07.java
+++ src/Lab07.java
@@ -1,10 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Lab07 {
     public static int[][] myArray = {{1,2,3},{4,5,6},{7,8,9}};
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         printArray(myArray);
         printArrayEven(myArray);
+
+        int[][] array = fillArray("twodimension.txt");
+        printArray(array);
     }
+
+
     private static void printArray (int[][] theArray) {
         for (int i=0; i<theArray.length; i++) {
             for (int j=0; j<theArray[i].length;j++)
@@ -27,10 +36,20 @@ public class Lab07 {
                 }
             }
         }
+        System.out.println();
     }
 
-    public static int[][] fillArray(String myFile) {
-        int myInt[][] = new int[0][];
-        return myInt;
+    public static int[][] fillArray(String myFile) throws FileNotFoundException {
+        File f = new File(myFile);
+        Scanner fileReader = new Scanner(f);
+        int r = Integer.parseInt(fileReader.nextLine());
+        int c = Integer.parseInt(fileReader.nextLine());
+        int[][] array = new int[r][c];
+        for (int i = 0; i < r; i++)
+            for (int j = 0; j < c; j++) {
+                array[i][j] = Integer.parseInt(fileReader.next());
+            }
+        fileReader.close();
+        return array;
     }
 }
