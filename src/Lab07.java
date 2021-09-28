2c5c260f5d7172175e9649510057e3d0e1992898
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-09-28 15:07:54 -0400

    Code added

--- src/Lab07.java
+++ src/Lab07.java
@@ -1,16 +1,17 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Scanner;
+
+// Muhammad Khan
 
 public class Lab07 {
     public static int[][] myArray = {{1,2,3},{4,5,6},{7,8,9}};
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws NumberFormatException, IOException {
         printArray(myArray);
         printArrayEven(myArray);
 
-        int[][] array = fillArray("twodimension.txt");
-        printArray(array);
+        int[][] array = fillArray(args[0]);
+        printArrayEven(array);
     }
 
 
@@ -27,29 +28,42 @@ public class Lab07 {
     }
     private static void printArrayEven(int[][] theArray){
         for (int i = 0; i < theArray.length; ++i) {
-            for (int j = 0; j < theArray.length; ++j) {
-                if (i % 2 != 0 || j % 2 != 0) {
+            for (int j = 0; j < theArray[i].length; ++j) {
+                if (theArray[i][j] % 2 == 0) {
                     display(theArray[i][j]);
                 }
                 else {
-                    System.out.println("* ");
+                    System.out.print("* ");
                 }
             }
+            System.out.println();
         }
         System.out.println();
     }
 
-    public static int[][] fillArray(String myFile) throws FileNotFoundException {
-        File f = new File(myFile);
-        Scanner fileReader = new Scanner(f);
-        int r = Integer.parseInt(fileReader.nextLine());
-        int c = Integer.parseInt(fileReader.nextLine());
-        int[][] array = new int[r][c];
-        for (int i = 0; i < r; i++)
-            for (int j = 0; j < c; j++) {
-                array[i][j] = Integer.parseInt(fileReader.next());
+    public static int[][] fillArray(String myFile) throws NumberFormatException, IOException {
+
+        // Open file
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(myFile)));
+
+        // read the first line to get the total row number
+        int row = Integer.parseInt(br.readLine());
+
+        // read 2nd line to get the total col num
+        int col = Integer.parseInt(br.readLine());
+
+        // generate 2-d array
+        int[][] myArray = new int[row][col];
+
+        // read the rest of the file and save into each element
+        for (int i = 0; i < myArray.length; i++) {
+            for (int j = 0; j < myArray[i].length; j++) {
+                myArray[i][j] = Integer.parseInt(br.readLine());
             }
-        fileReader.close();
-        return array;
+        }
+        // close the file
+        br.close();
+
+        return myArray;
     }
 }
