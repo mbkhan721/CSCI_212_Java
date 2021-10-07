92ecaf38db8a2face6279315dcbc1f1c33e118cf
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-10-07 00:12:20 -0400

    Code added

--- src/TextFileInput/Lab08.java
+++ src/TextFileInput/Lab08.java
@@ -1,25 +1,16 @@
 // Muhammad Khan
 
 package TextFileInput;
-import TextFileInput.TextFileInput;
 
 import java.io.*;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Lab08 {
     public static TextFileInput myFile;
     public static StringTokenizer myTokens;
-    public static String animal;
     public static String[] animals;
     public static String line;
-
-    // HW
-    public static TextFileInput myArray;
-    public static String firstLine;
-    public static StringTokenizer twoDimArrayToken;
-    public static String[] twoDimArray;
-    public static String array;
-
 
 
     public static void main(String[] args) throws IOException {
@@ -57,51 +48,29 @@ public class Lab08 {
             System.out.println(animals[j]);
 
         // Homework
-//        myFile = new TextFileInput("twodimension8.txt");
-//        firstLine = myFile.readLine();
-//        myTokens = new StringTokenizer(firstLine, ",");
-//        System.out.println("\nShould print first line " + firstLine);
+        File myArray = new File("twodimension8.txt");
+        Scanner read = new Scanner(myArray);
 
-        myArray = new TextFileInput("twodimension8.txt");
-        array = myArray.readLine();
-        twoDimArrayToken = new StringTokenizer(array, ",");
-        twoDimArray = new String[twoDimArrayToken.countTokens()];
-        int firstLine = 0;
-        while (twoDimArrayToken.hasMoreTokens()) {
-            twoDimArray[firstLine] = twoDimArrayToken.nextToken();
-            firstLine++;
+        String[] arr;
+        arr = read.nextLine().split(",");
+        int row = Integer.parseInt(arr[0]);
+        int col = Integer.parseInt(arr[1]);
+        int[][] a = new int[row][col];
+
+        for (int j = 0; j < row; j++) {
+            arr = read.nextLine().split(",");
+            for (int k = 0; k < col; k++) {
+                a[j][k] = Integer.parseInt(arr[k]);
+            }
         }
-        int row = 0;
-        int col = 0;
-        for (int j = 0; j < twoDimArray.length; j++) {
-            row = Integer.parseInt(twoDimArray[j]);
-            col = Integer.parseInt(twoDimArray[j]);
+        read.close();
+
+        System.out.println();
+        for (int j = 0; j < row; j++) {
+            for (int k = 0; k < col; k++) {
+                System.out.print(a[j][k] + " ");
+            }
+            System.out.println();
         }
-        //System.out.println("\nExp: " + row + " " + col);
-
-
-//        int[][] a = readArray(args[0]);
-//        for (int j = 0; j < a.length; j++) {
-//            System.out.println(a[j]);
-//        }
-    } //main
-
-//    public static int[][] readArray(String file) throws IOException {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
-//        int row = Integer.parseInt(br.readLine());
-//        int col = Integer.parseInt(br.readLine());
-//
-//        int[][] a = new int[row][col];
-//        for (int i = 0; i < a.length; i++) {
-//            for (int j = 0; j < a[i].length; j++) {
-//                a[i][j] = Integer.parseInt(br.readLine());
-//
-//            }
-//        }
-//        br.close();
-//        return a;
-//
-//    }
-
-    // Lab 5 or lab 6 for reference to read text.input class
+    }
 }
