ffabaac246ccf81808de52a027a6925d6f86b785
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-10-08 18:37:19 -0400

    Code added

--- src/Lab10_Program_JavaDoc/Lab10.java
+++ src/Lab10_Program_JavaDoc/Lab10.java
@@ -1,12 +1,36 @@
+package Lab10_Program_JavaDoc;
 // Muhammad Khan
+// Lab 10
+
+import java.util.Arrays;
+
+/***************************************************************
+ *
+ * @author K. Lord
+ * A program that works with two-dimension arrays.
+ *
+ */
 
 public class Lab10 {
     private static int[][] matrix;
 
     public static void main(String[] args) {
+
         matrix = new int[6][4];
         fillMatrix(matrix, 5);
         System.out.println(sumMatrix(matrix));
+
+        int[][] matrixSeq = new int[2][4];
+        int v = 2;
+        fillMatrixSeq(matrixSeq, 4);
+        System.out.println("\nMatrix Sequence: ");
+        System.out.println(Arrays.deepToString(matrixSeq));
+
+
+        int[][] addMatrixRow = new int[3][4];
+        sumMatrixRow(addMatrixRow);
+        System.out.println("\nSum Matrix Row: ");
+        System.out.println(Arrays.deepToString(addMatrixRow));
     }
 
     /**
@@ -22,6 +46,11 @@ public class Lab10 {
                 m[i][j] = v;
     }
 
+    /**
+     * Used for filling the matrix
+     * @param m The 2-D array parameter size
+     * @param v int parameter value to be passed on to this function
+     */
     public static void fillMatrixSeq(int[][] m, int v) {
         int seqValue = 0;
         for (int i = 0; i < m.length; i++)
@@ -29,6 +58,11 @@ public class Lab10 {
                 m[i][j] = seqValue++;
     }
 
+    /**
+     * Return sum of the matrix
+     * @param m The matrix to be summed
+     * @return int the sum value of the matrix
+     */
     public static int sumMatrix(int[][] m) {
         int sum = 0;
         for (int i = 0; i < m.length; i++)
@@ -37,6 +71,11 @@ public class Lab10 {
         return sum;
     }
 
+    /**
+     * This is a comment added by Muhammad Khan
+     * @param m This function returns the sum of row
+     * @return integer value of the row
+     */
     public static int sumMatrixRow(int[][] m) {
         int sum = 0;
         for (int i = 0; i < m.length; i++)
---
+++ src/Lab10_Program_JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"Lab10_Program_JavaDoc","l":"Lab10"}]
