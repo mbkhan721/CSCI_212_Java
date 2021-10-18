1c99393c928bdee3093d184bd647a0c0deb245aa
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-10-18 13:22:39 -0400

    Code added

---
+++ src/Book.java
@@ -0,0 +1,38 @@
+
+public class Book {
+    private String title, author;
+    private float price;
+
+    public Book(String t, String a, float p) {
+
+        if (t == null) {
+            throw new IllegalArgumentException("Incorrect title");
+        }
+        if (a == null) {
+            throw new IllegalArgumentException("Incorrect author");
+        }
+        if (p < 0) {
+            throw new IllegalArgumentException("Incorrect price");
+        }
+
+        title = t;
+        author = a;
+        price = p;
+    }
+
+    // Accessors
+    public String getTitle() {return title;}
+    public String getAuthor() {return author;}
+    public float getPrice() {return price;}
+
+    // Mutators
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
--- src/PracticeExam01.java
+++ src/PracticeExam01.java
@@ -16,16 +16,34 @@ public class PracticeExam01 {
 //            throw new IllegalArgumentException();
 //        }
 
+
         // --------------------------------------------- Q2
+
         int arr[][] = {{10, 45}, {2, 42, 67, 5}, {3, 21}};
         System.out.println("\nSum = "+ getSumOfRow(arr, 1));
 
-    }
+
+        // --------------------------------------------- Q3
+
+        String inputStr;
+        inputStr = JOptionPane.showInputDialog(null,
+                "Enter anything: ");
+        int num = 0;
+        for (int i = 0; i < inputStr.length(); i++) {
+            if (Character.isDigit(inputStr.charAt(i))) {
+                ++num;
+            }
+        }
+        JOptionPane.showMessageDialog(null,
+                "Num of digits in your input is " + num);
+    } // main
+
+    // --------------------------------------------- Q2
     private static int getSumOfRow (int [][] myArray, int row) {
         int sum = 0;
         for (int i = 0; i < myArray[row].length; i++) {
                 sum += myArray[row][i];
         }
         return sum;
-    } // getSumOfRow
+    }
 }
