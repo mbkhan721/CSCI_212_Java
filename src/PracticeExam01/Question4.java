8b3dde92605d73445d093791bcd8f0f9898a7cd0
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-10-19 19:58:02 -0400

    Code added

--- src/PracticeExam01/Question4.java
+++ src/PracticeExam01/Question4.java
@@ -13,21 +13,21 @@ public class Question4 {
         System.out.println(mixThem(s2,s3));
     } //main
 
-    private static void animal (String a1, String a2) {
+    private static void animal (String a1, String a2) { // CAT DOG
         if(a1.equals(a2))
-            System.out.println(a1);
+            System.out.println(a1);                     // Prints DOG
         else
             System.out.println(a2);
     }
 
-    private static String mixThem (String b1, String b2){
-        String c1 = b1;
-        b1 = b2;
-        b2 = new String ("HAT");
-        System.out.println(b1);
-        System.out.println(b2);
-        System.out.println(c1);
-        System.out.println(s2);
-        return b2;
+    private static String mixThem (String b1, String b2){ // DOG RAT
+        String c1 = b1;                                     // c1 = DOG, temp
+        b1 = b2;                                            // b1 = RAT
+        b2 = new String ("HAT");                    // b2 = HAT
+        System.out.println(b1);                             // RAT
+        System.out.println(b2);                             // HAT
+        System.out.println(c1);                             // DOG
+        System.out.println(s2);                             // DOG
+        return b2;                                          // HAT
     }
 }
