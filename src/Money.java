869aa48a43b11d244f84c0ec6acededa2872c97b
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-10-23 22:32:40 -0400

    Code added

--- src/Money.java
+++ src/Money.java
@@ -2,4 +2,42 @@
 // Lab 14
 
 public class Money {
+
+    int dollars, cents;
+
+    public Money() {
+        dollars = 0;
+        cents = 0;
+    }
+
+    public Money(int d, int c) {
+        dollars = d;
+        cents = c;
+    }
+
+    public int getDollars() {
+        return dollars;
+    }
+    public int getCents() {
+        return cents;
+    }
+
+    @Override
+    public String toString() {
+        return "$ " + dollars + "." + cents;
+    }
+
+    public int compareTo(Money m) {
+        return ("$ " + dollars + "." + cents).compareTo("$ " + m.dollars + "." + m.cents);
+    }
+
+    public static void main(String []args) {
+        Money m1 = new Money();
+        Money m2 = new Money(6,5);
+        System.out.println(m1.getCents());
+        System.out.println(m2.getDollars());
+        System.out.println(m2);
+        System.out.println(m1.compareTo(m2));
+        System.out.println(m1.equals(m2));
+    }
 }
