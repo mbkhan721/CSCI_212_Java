57e8cef4696ad0e9692bb6cc96c9a77bc7ac51b0
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-11-07 21:38:14 -0500

    Code added

---
+++ src/Lab18/MoneyNode.java
@@ -0,0 +1,12 @@
+package Lab18;
+
+public class MoneyNode {
+
+    public String data;
+    public MoneyNode next;
+
+    public MoneyNode(String d) {
+        data = d;
+        next = null;
+    }  // constructor
+}
