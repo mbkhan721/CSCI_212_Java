3d66e6431683ce3fb6cc3e63c8f2be020ec8c6dc
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-11-12 01:51:28 -0500

    Code added

---
+++ src/Project2/Date212Node.java
@@ -0,0 +1,11 @@
+package Project2;
+
+public class Date212Node {
+    //protected Date212 data;
+    protected Date212Node next;
+
+    public Date212Node() { // Include a one-argument constructor which takes a (Date212 d) as a parameter
+        //this.data = d;
+        this.next = null;
+    }
+}
