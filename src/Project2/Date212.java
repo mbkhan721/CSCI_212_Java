4f1baa582a79fe1fa6130e336a29cbe7e09d2f1f
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-11-12 02:04:35 -0500

    Code added

---
+++ src/Project2/Date212.java
@@ -0,0 +1,4 @@
+package Project2;
+
+public class Date212 {
+}
---
+++ src/Project2/Date212List.java
@@ -0,0 +1,10 @@
+package Project2;
+
+public class Date212List {
+
+    private Date212Node head = new Date212Node(null);
+    private Date212Node last = head;
+    private int length = 0;
+
+
+}
