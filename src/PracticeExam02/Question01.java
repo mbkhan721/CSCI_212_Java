5849a1c0535724767bce0e6dad3ca1ea002c71dc
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-11-23 00:29:24 -0500

    Code added

--- src/PracticeExam02/Question01.java
+++ src/PracticeExam02/Question01.java
@@ -1,4 +1,44 @@
 package PracticeExam02;
 
 public class Question01 {
+    int data;
+    Question01 next;
 }
+
+
+/*
+1. Define a list node
+It's a dynamic data structure that links the item in a list to another. A linked list is composed of
+objects known as nodes. Each  node allocates memory to hold data and a next to hold link to another node.
+The links are implemented as references to the node. In a linkedList, each node only contains one link.
+
+2. Define a singly-linked list with head node and counter
+It's a data structure in which each element of the list contains a pointer. The first node of the list is
+called as head. Counter count the number of occurrences in the node. You first initialize counter to 0,
+loop through each element of the list, if element data is equal to the passed number then increment the
+counter.
+
+3. Be able to traverse the list from beginning to end and, for example, find a given node, print the data
+in a given node.
+*/
+class LinkedList {
+    Question01 head;
+    public int givenNode(int index) {
+        Question01 current = head; // head of the list
+        int count = 0; // index of node we are currently looking at.
+        while (current != null) {
+            if (count == index)
+                return current.data;
+            count++;
+            current = current.next;
+        }
+        assert (false); // if we get to this line, the caller was askin for a non-existent element so
+                        // we assert fail
+        return 0;
+    }
+}
+
+/*
+4. Append a new node to the end of the list.
+
+ */
