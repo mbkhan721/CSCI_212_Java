0661103d7ed8abb0bfcf3eff2438e5d0ac30992a
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-11-24 19:52:05 -0500

    Code added

---
+++ src/PracticeExam02/Inheritance03.java
@@ -0,0 +1,13 @@
+package PracticeExam02;
+
+public class Inheritance03 {
+}
+
+/*
+1 - Understand what methods and data items are inherited from a super class when they are declared public,
+private and protected
+
+ */
+
+
+
