8ed255383541072a5fb37abd0ce0e7c3db1d8225
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-11-26 23:08:06 -0500

    Code added

--- src/PracticeExam02/Inheritance/IsADemo.java
+++ src/PracticeExam02/Inheritance/IsADemo.java
@@ -1,4 +1,25 @@
 package PracticeExam02.Inheritance;
 
 public class IsADemo {
+
+    public static void main(String[] args) {
+        SalariedEmployee joe = new SalariedEmployee("Josephine",
+                new Date("January", 1, 1995), 10000);
+
+        HourlyEmployee sam = new HourlyEmployee("Sam",
+                new Date("February", 1, 1996), 50.50, 40);
+
+        System.out.println("\nJoe's longer name is " + joe.getName());
+        System.out.println("showEmployee(joe) invoked:");
+        showEmployee(joe);
+
+        System.out.println("\nshowEmployee(sam) invoked:");
+        showEmployee(sam);
+
+    }
+
+    public static void showEmployee(Employee employeeObject) {
+        System.out.println(employeeObject.getName());
+        System.out.println(employeeObject.getHireDate());
+    }
 }
