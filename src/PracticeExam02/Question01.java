f334bc1627be070d3e6e09d0c065b0251e047600
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-11-29 12:32:56 -0500

    Code added

--- src/PracticeExam02/Question01.java
+++ src/PracticeExam02/Question01.java
@@ -1,6 +1,47 @@
 package PracticeExam02;
 
 public class Question01 {
+
+    // SSN class ---------------------------------------------------------|
+//    public class SSN {
+//
+//        private String item;
+//        private int count;
+//        private SSN link; // a node contains reference to another node, that reference
+//                          // is a link to next node.
+//
+//        private SSN() {
+//            link = null;
+//            item = null;
+//            count = 0;
+//        }
+//
+//
+//    public String getItem() {
+//        return item;
+//    }
+//    public int getCount() {
+//        return count;
+//    }
+//    public SSN getLink() {
+//        return link;
+//    }
+
+
+    // SSNList class ---------------------------------------------------------|
+
+//    public void PrintSSN() {
+//        SSN position = head;  // position is the name for the node
+//
+//        while (position != null) {
+//            System.out.println(position.getItem() + " " + position.getCount());
+//            position = position.getLink();  // position.link is a name for the link to
+//            // the next node. The value of link is produced
+//            // with accessor method getLink().
+//        }
+//    }
+
+
 }
 
 
