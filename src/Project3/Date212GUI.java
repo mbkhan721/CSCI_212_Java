fedb19eb617f8467cf702ff7243837a8e235386a
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-12-01 21:57:18 -0500

    Code added

--- src/Project3/Date212GUI.java
+++ src/Project3/Date212GUI.java
@@ -1,15 +1,9 @@
 package Project3;
 
-// Muhammad Khan
-// Project 2
-// CSCI 212 11
-
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class Date212GUI extends JFrame implements ActionListener {
+public class Date212GUI {
 
     TextArea unsortedDates, sortedDates;
 
@@ -26,23 +20,6 @@ public class Date212GUI extends JFrame implements ActionListener {
         frame.getContentPane().add(unsortedDates);
         frame.getContentPane().add(sortedDates);
         frame.setVisible(true);
-
-        JMenu optionMenu = new JMenu("File");
-        JMenuItem openChoice = new JMenuItem("Open");
-        openChoice.addActionListener(this);
-        optionMenu.add(openChoice);
-
-        JMenuItem quitChoice = new JMenuItem("Quit");
-        quitChoice.addActionListener(this);
-        optionMenu.add(quitChoice);
     }
 
-    public void actionPerformed(ActionEvent e) {
-        String action = e.getActionCommand();
-
-        if (action.equals("Open"))
-            openFile();
-        else if (action.equals("Quit"))
-            System.exit(0);
-    }
 }
