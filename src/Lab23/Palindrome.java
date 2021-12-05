6af188cea54684e4c3b60bb9d49c4861a2ac7317
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-12-04 23:31:30 -0500

    Code added.

--- src/Lab23/Palindrome.java
+++ src/Lab23/Palindrome.java
@@ -18,8 +18,28 @@ public class Palindrome {
     }
 
     private static boolean isPalindromeHelper(String in) {
-        // Fill me in
-        return false;
+        // base case
+        if (in.length() <= 1) // if in is a single char, or ""
+            return true;
+
+        // use variables to save the first char and last char
+        char first = in.charAt(0);
+        char last = in.charAt(in.length() - 1);
+
+        // check if the first and last is letter or not
+        if (!Character.isLetter(first)) {
+            return isPalindromeHelper(in.substring(1));
+        }
+        else if (!Character.isLetter(last)) {
+            return isPalindromeHelper(in.substring(0, in.length() - 1));
+        }
+
+        // if the first char not equal to the last char
+        if (Character.toLowerCase(first) != Character.toLowerCase(last))
+            return false;
+
+        // if equals
+        return isPalindromeHelper(in.substring(1, in.length() - 1));
 
     }
 }
