d4180fcc8fb86bb4bd5e26577814bb608991bd32
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-12-05 00:17:19 -0500

    Lab 25 Started

---
+++ src/Lab25/Lab25.java
@@ -0,0 +1,34 @@
+package Lab25;
+
+public class Lab25 {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int[] intArray = { 3, 5, 9, 4, 10, 33, 12, 19, 4, 16, 23, 30 };
+        int[] intArray1 = { 1, 2, 4, 5, 5, 6, 7, 8, 9, 100 };
+
+        System.out.println(largest(intArray, 0));
+        System.out.println(sum(intArray, 0));
+        System.out.println(isSorted(intArray, 0));
+        System.out.println(isSorted(intArray, 8));
+        System.out.println(isSorted(intArray1, 0));
+    }
+
+    public static int largest(int[] myArray, int index) {
+        // TODO Fill me in
+        return 0;
+    }
+
+    public static int sum(int[] myArray, int index) {
+        // TODO Fill me in
+        return 0;
+    }
+
+    public static boolean isSorted(int[] myArray, int index) {
+        // TODO Fill me in
+        return false;
+    }
+
+}
