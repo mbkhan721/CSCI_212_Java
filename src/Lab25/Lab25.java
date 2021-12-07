19f363cccd8656d368f877e57effe0c16aa758df
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-12-07 12:06:48 -0500

    Code added

--- src/Lab25/Lab25.java
+++ src/Lab25/Lab25.java
@@ -9,26 +9,32 @@ public class Lab25 {
         int[] intArray = { 3, 5, 9, 4, 10, 33, 12, 19, 4, 16, 23, 30 };
         int[] intArray1 = { 1, 2, 4, 5, 5, 6, 7, 8, 9, 100 };
 
-        System.out.println(largest(intArray, 0));
-        System.out.println(sum(intArray, 0));
-        System.out.println(isSorted(intArray, 0));
-        System.out.println(isSorted(intArray, 8));
-        System.out.println(isSorted(intArray1, 0));
+        System.out.println(largest(intArray, intArray.length - 1));
+        System.out.println(sum(intArray, intArray.length - 1));
+        System.out.println(isSorted(intArray, intArray.length - 1));
+        System.out.println(isSorted(intArray1, intArray1.length - 1));
     }
 
     public static int largest(int[] myArray, int index) {
-        // TODO Fill me in
-        return 0;
+        if (index > 0) {
+            return Math.max(myArray[index],largest(myArray, index - 1));
+        }
+        else
+            return myArray[0];
     }
 
     public static int sum(int[] myArray, int index) {
-        // TODO Fill me in
-        return 0;
+        if (index <= 0) {
+            return 0;
+        }
+        return sum(myArray, index - 1) + myArray[index - 1];
     }
 
     public static boolean isSorted(int[] myArray, int index) {
-        // TODO Fill me in
-        return false;
+        if (index == 1 || index == 0) {
+            return true;
+        }
+        return myArray[index - 1] >= myArray[index - 2] && isSorted(myArray, index - 1);
     }
 
 }
