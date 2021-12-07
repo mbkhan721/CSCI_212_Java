807d7fd308a94305ea86e2442bdad3887fc45e4f
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-12-07 14:13:17 -0500

    Code added

--- src/Lab25/Lab25.java
+++ src/Lab25/Lab25.java
@@ -1,20 +1,26 @@
 package Lab25;
 
 public class Lab25 {
-    /**
-     * @param args
-     */
+
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         int[] intArray = { 3, 5, 9, 4, 10, 33, 12, 19, 4, 16, 23, 30 };
         int[] intArray1 = { 1, 2, 4, 5, 5, 6, 7, 8, 9, 100 };
 
-        System.out.println(largest(intArray, intArray.length - 1));
-        System.out.println(sum(intArray, intArray.length - 1));
-        System.out.println(isSorted(intArray, intArray.length - 1));
-        System.out.println(isSorted(intArray1, intArray1.length - 1));
 
-        // largest array without recursive function.
+        int large = largest(intArray, 0);
+        System.out.println("Largest number in intArray is " + large);
+
+        int sum = sum(intArray, 0);
+        System.out.println("The sum of intArray is " + sum);
+
+        boolean sort = isSorted(intArray, 0);
+        System.out.println("Is intArray sorted? " + sort);
+
+        sort = isSorted(intArray1, 0);
+        System.out.println("Is intArray1 sorted? " + sort);
+
+        // largest number in array without recursive function.
 //        int max = intArray[0];
 //        for (int i = 0; i < intArray.length; i++) {
 //            if (intArray[i] > max)
@@ -23,25 +29,45 @@ public class Lab25 {
     }
 
     public static int largest(int[] myArray, int index) {
-        if (index > 0) {
-            return Math.max(myArray[index],largest(myArray, index - 1));
-        }
-        else
-            return myArray[0];
+        // base case
+//
+//        if (index >= myArray.length)
+//            return Integer.MIN_VALUE;
+
+        if (index >= myArray.length - 1)
+            return myArray[myArray.length - 1];
+
+        return Math.max(myArray[index], largest(myArray, index + 1));
+//        if (index > 0) {
+//            return Math.max(myArray[index],largest(myArray, index - 1));
+//        }
+//        else
+//            return myArray[0];
     }
 
     public static int sum(int[] myArray, int index) {
-        if (index <= 0) {
+//        if (index <= 0) {
+//            return 0;
+//        }
+//        return sum(myArray, index - 1) + myArray[index - 1];
+
+        if (index >= myArray.length)
             return 0;
-        }
-        return sum(myArray, index - 1) + myArray[index - 1];
+
+        return myArray[index] + sum(myArray, index + 1);
+
     }
 
     public static boolean isSorted(int[] myArray, int index) {
-        if (index == 1 || index == 0) {
+//        if (index == 1 || index == 0) {
+//            return true;
+//        }
+//        return myArray[index - 1] >= myArray[index - 2] && isSorted(myArray, index - 1);
+
+        if (index + 1 >= myArray.length)
             return true;
-        }
-        return myArray[index - 1] >= myArray[index - 2] && isSorted(myArray, index - 1);
+
+        return myArray[index] <= myArray[index + 1] && isSorted(myArray, index + 1);
     }
 
 }
