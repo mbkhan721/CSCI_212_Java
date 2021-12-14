371ba9829ed7075f84951afe2a5a934f188b7161
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2021-12-14 11:59:41 -0500

    Code added

--- src/Generics/ArrayListDemo.java
+++ src/Generics/ArrayListDemo.java
@@ -1,5 +1,33 @@
 package Generics;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ArrayListDemo {
-    // pg 775
+
+    public static void main(String[] args) {
+
+        ArrayList<String> toDoList = new ArrayList<String>(20);
+        System.out.println("Enter list entries, when prompted.");
+
+        boolean done = false;
+        String next = null;
+        String answer;
+        Scanner keyboard = new Scanner(System.in);
+
+        while (!done) {
+            System.out.println("Input an entry:");
+            next = keyboard.nextLine();
+            toDoList.add(next);
+            System.out.println("More items for the list?");
+            answer = keyboard.nextLine();
+
+            if (!(answer.equalsIgnoreCase("yes")))
+                done = true;
+        }
+
+        System.out.println("The list contains:");
+        for (String entry : toDoList)
+            System.out.println(entry);
+    }
 }
